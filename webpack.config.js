02ba2b41290449855b5d2be331741da71bf4eec0
Vladislav <vladislav.work98@gmail.com>
2023-11-28 10:30:26 +0200

    set up image optimization

--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,25 @@ module.exports = {
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
+
+        assetModuleFilename: (pathData) => {
+            const filepath = path
+                .dirname(pathData.filename)
+                .split('/')
+                .slice(1)
+                .join('/');
+            return `${filepath}/[name][ext]`;
+        },
     },
     module: {
         rules: [
             {
                 test: /\.css$/i,
                 use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
             },
         ],
     },
